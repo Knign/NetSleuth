e656f2c56f39806216c2512808e546c41e084330
Kunal Walavalkar <kunalw2002@gmail.com>
2023-01-07 23:11:22 +0530

    Added if __name__=='__main__'

--- netscanner.py
+++ netscanner.py
@@ -11,31 +11,37 @@ parser.add_argument("ip", help="IP to scan")
 args = parser.parse_args()
 target_ip = str(args.ip)
 
-try:
-    # create ARP packet
-    arp = ARP(pdst=target_ip)
+def scan_network():
+    try:
+        # create ARP packet
+        arp = ARP(pdst=target_ip)
 
-    # create the Ether broadcast packet
-    # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    # stack them
-    packet = ether/arp
+        # create the Ether broadcast packet
+        # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+        # stack them
+        packet = ether/arp
 
-    result = srp(packet, timeout=3, verbose=0)[0]
+        result = srp(packet, timeout=3, verbose=0)[0]
 
-    # Listing the clients
-    clients = []
+        # Listing the clients
+        clients = []
 
-    for sent, received in result:
-        # for each response, append ip and mac address to `clients` list
-        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+        for sent, received in result:
+            # for each response, append ip and mac address to `clients` list
+            clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-    # Printing clients
-    print("Available devices in the network:")
-    print("IP" + " "*18+"MAC")
-    for client in clients:
-        print("{:16}    {}".format(client['ip'], client['mac']))
+        # Printing clients
+        print("Available devices in the network:")
+        print("IP" + " "*18 + "MAC")
+        print("-"*40)
 
-except KeyboardInterrupt:
-    print("\nExiting Program")
-    sys.exit()
+        for client in clients:
+            print("{:16}    {}".format(client['ip'], client['mac']))
+
+    except KeyboardInterrupt:
+        print("\nExiting Program")
+        sys.exit()
+
+if __name__ == '__main__':
+    scan_network()
